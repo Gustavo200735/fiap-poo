b2fd13346e25e37b9f28bae3414d551722c4a8fb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-24 21:28:31 -0300

    Aula 1 - Fundamentos de OO - Classes e Objetos

---
+++ br/com/fiapride/model/Fones.java
@@ -0,0 +1,7 @@
+package br.com.fiapride.model;
+public class Fones { // <-- Use o nome do SEU objeto    
+    // As características que você pensou    
+    public String cor;
+    public String nomeDispositivo;
+    
+}
